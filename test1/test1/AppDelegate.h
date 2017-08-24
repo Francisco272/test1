36b9fc0516aa2a9f487b6272b9311116f2f5988c
Francisco <francisco@FranciscodeMacBook-Pro.local>
2017-08-24 20:56:27 +0800

    first commit

---
+++ test1/test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test1
+//
+//  Created by Francisco on 2017/8/24.
+//  Copyright © 2017年 Francisco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test1/test1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test1
+//
+//  Created by Francisco on 2017/8/24.
+//  Copyright © 2017年 Francisco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
